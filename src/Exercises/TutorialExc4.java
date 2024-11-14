cf27cd37ed89c3f0cc9b492136f0511a99cf2bff
Danial <danialharith661@gmail.com>
2024-11-14 17:31:17 +0800

    question1 tut4

---
+++ src/Exercises/TutorialExc4.java
@@ -0,0 +1,24 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class TutorialExc4 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        final double DISCOUNT = 0.9;
+        double Total=0,x=0;
+
+        do{
+            System.out.print("Enter the price of the item (0 to finish): ");
+            x = input.nextInt();
+
+            if(x>=100){
+                x *= DISCOUNT;
+            }
+            System.out.println(x);
+            Total += x;
+        }while(x!=0);
+
+        System.out.println("Total cost of items in the cart: " + Total);
+    }
+}
