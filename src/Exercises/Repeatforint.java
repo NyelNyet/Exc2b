e988cd1cd81018719691a79888bd9c45dc67f079
Danial <danialharith661@gmail.com>
2024-11-20 12:49:48 +0800

    RepeatforVoidorInt

---
+++ src/Exercises/Repeatforint.java
@@ -0,0 +1,26 @@
+package Exercises;
+import java.util.Scanner;
+public class Repeatforint {
+    public static void main(String[] args){
+        System.out.println("This program accumulate the total number of bricks");
+        System.out.println("for a pyramid with a square base, where each layer uses");
+        System.out.println("layer X layer bricks where the total layer required is");
+        System.out.println("decided by the user");
+
+        System.out.println("Total bricks needed is : " + totalbricks());
+    }
+
+    public static int totalbricks(){
+        int totalbricks = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter total number of layers: ");
+        int totalLayers = input.nextInt();
+
+        for(int layer = 1; layer <= totalLayers; layer++){
+            totalbricks += layer*layer;
+        }
+        
+        input.close();
+        return totalbricks;
+    }
+}
