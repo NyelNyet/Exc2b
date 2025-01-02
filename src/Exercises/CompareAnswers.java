66379cf7587f356bd01855b090f60e3d7bfeaec5
Danial <danialharith661@gmail.com>
2025-01-02 16:56:24 +0800

    2/1 exceptions excercise

--- src/Exercises/CompareAnswers.java
+++ src/Exercises/CompareAnswers.java
@@ -1,8 +1,11 @@
 package Exercises;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CompareAnswers {
     public static void main(String[] args){
-        char[] answers = {'D','B','D','C','C','D','A','E','A','D'};
+        char[] answers = new char[10];
         char[][] studAns = {
             {'A','B','A','C','C','D','E','E','A','D'},
             {'D','B','A','B','C','A','E','E','A','D'},
@@ -13,6 +16,19 @@ public class CompareAnswers {
             {'B','B','A','C','C','D','E','E','A','D'},
             {'E','B','E','C','C','D','E','E','A','D'},
         };
+
+        System.out.println("Enter answer scheme for "+answers.length+" questions: ");
+        try{
+            getanswers(answers);
+        }catch(InputMismatchException ime){
+            System.out.println(ime.getMessage());
+            return;
+        }catch(ArrayIndexOutOfBoundsException aioobe){
+            System.out.println("You've entered more than you're supposed to!");
+            return;
+        }finally{
+            System.out.println("Exception handled");
+        }
 
         int[] marks = new int[8];
         
@@ -35,6 +51,18 @@ public class CompareAnswers {
     public static void printmarks(int[] marks){
         for(int i = 0;i<marks.length;i++){
             System.out.println("Student "+i+" got "+marks[i]+" marks");
+        }
+    }
+
+    public static void getanswers(char[] answers){
+        Scanner input = new Scanner(System.in);
+        for(int i = 0;i<answers.length+1;i++){
+            String tempans = input.nextLine();
+            answers[i] = tempans.toUpperCase().charAt(0);
+            System.out.println(answers[i]);
+            if(Character.isLetter(answers[i])==false){
+                throw new InputMismatchException("Invalid integer input!");
+            }
         }
     }
 }
