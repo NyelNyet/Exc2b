f938e5dc2c14fb00a779dd70cd7fba4c8e03c13b
Danial <danialharith661@gmail.com>
2025-01-31 17:35:32 +0800

    jdbc

---
+++ src/JDBC/Connecttry.java
@@ -0,0 +1,49 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connecttry {
+    public static void main(String[] args){
+
+        Connection connection = null;
+
+        try{
+            // Registering Oracle Driver Class
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Getting Connection
+            connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe",
+                "user1",
+                "user1");
+
+            // Test If Connection Is NULL Or Not
+            if(connection != null){
+                System.out.println("Connection established successfully!");
+            }else{
+                System.out.println("No Connection!");
+            }
+
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+            
+        }finally{
+            try{
+
+                if(connection != null){
+                    connection.close();
+                }
+
+            }catch(SQLException e){
+                    e.printStackTrace();
+                    
+                }
+        }
+    }
+}
