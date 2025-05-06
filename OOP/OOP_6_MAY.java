0c58d42c944006710f7ba9e4b69f8824a4af0ac0
Danial <danialharith661@gmail.com>
2025-05-06 17:04:09 +0800

    OOP 6 April

--- OOP/OOP_6_MAY.java
+++ OOP/OOP_6_MAY.java
@@ -3,15 +3,26 @@ import java.util.*;
 
 public class OOP_6_MAY {
 
+    //Text colouring
+    private static String[] bold = {"\033[1m","\033[0m"};
+    private static String green = "\033[32m";
+    private static String resCol = "\033[39m";
+    private static String[] underline = {"\033[4m","\033[0m"};
+
+    //set for the whole class since im using 2 method that will use the same ArrayList and File name
     private static File f = new File("Student.txt");
+
     public static void main(String[] args){
         printWriterMethod(getStudName());
 
-        //fileWriterMethod(getStudName());
+        System.out.println();//Output aesthetic purposes
+
+        fileWriterMethod(getStudName());
     }
 
     public static void fileWriterMethod(List<String> students){
         //Implementing FileWriter, FileReader, and BufferedReader
+        System.out.println(underline[0]+bold[0]+green+ "File Writer/Reader and BufferedReader Method:-"+resCol+bold[1]+underline[1]);
 
         try{
             FileWriter writer = new FileWriter(f);
@@ -42,6 +53,7 @@ public class OOP_6_MAY {
 
     public static void printWriterMethod(List<String> students){
         //Implementing PrintWriter and Scanner
+        System.out.println(underline[0]+bold[0]+green+ "Print Writer Method:-"+resCol+bold[1]+underline[1]);
 
         try{
             PrintWriter out = new PrintWriter(f);
@@ -67,12 +79,13 @@ public class OOP_6_MAY {
         } 
     }
 
+    //Tell user to input students name
     public static List<String> getStudName(){
         Scanner input = new Scanner(System.in);
         ArrayList<String> students = new ArrayList<>();
         String name;
 
-        while(true){
+        do{
             System.out.print("Enter Student Name(Enter 0 to exit): ");
             name = input.nextLine();
             if(name.equals("0")){
@@ -80,9 +93,7 @@ public class OOP_6_MAY {
             }else{
                 students.add(name);
             }
-        }
-
-        input.close();
+        }while(true);
 
         System.out.println();//Output aesthetic purposes
         return students;
